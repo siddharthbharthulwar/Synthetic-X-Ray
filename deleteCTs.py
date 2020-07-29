eb3deff1dcfbeef2cf1d4dd79bbe667f315e0ea0
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-07-29 11:31:56 -0600

    delete CTs with aliasing issue

--- deleteCTs.py
+++ deleteCTs.py
@@ -1,2 +1,17 @@
 import os
+import numpy as np
 
+CTDirlist = os.listdir(r"Data\Out")
+XrayDirList = os.listdir(r"CXR")
+
+CTDirlistNew = []
+for i in CTDirlist:
+
+    CTDirlistNew.append(i[0:4])
+
+main_list = np.setdiff1d(CTDirlistNew, XrayDirList)
+
+for item in main_list:
+
+    path = os.path.join(r"Data\Out", item + ".npy")
+    print(path)
