213328924ba1ff82ab6409b90ece82b1a8c91e8d
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-08-19 15:26:19 -0600

    add new training ipynb and quad dataset class

--- dataset.py
+++ dataset.py
@@ -131,7 +131,7 @@ class PairedDatasetSingle:
 
     def prepare(self):
         
-        self.x_train = np.reshape(self.x_train, (len(self.x_train), 256, 256, 1))
+        self.x_train = np.reshape(self.x_train, (len(self.x_train), 1024, 1024, 1))
         self.y_train = np.reshape(self.y_train, (len(self.y_train), 128, 128, 128, 1))
         print("Shape of X: {}".format(self.x_train.shape))
         print("Shape of Y: {}".format(self.y_train.shape))
@@ -196,24 +196,25 @@ class PairedDatasetDouble: #dataset for double view neural network model
         print("Length of x_train 1 files: {}".format(len(self.x_train_1_files)))
         print("Length of y_train files: {}".format(len(self.y_train_files)))
 
-        '''
+        
 
-        main_list = np.setdiff1d(self.x_train_files, self.y_train_files)
+        main_list = np.setdiff1d(self.x_train_0_files, self.y_train_files)
 
         for item in main_list:
 
             if (item[0] != '.'):
 
-                if item in x_train_files:
+                if item in self.x_train_0_files:
 
-                    os.remove(os.path.join(in_path, item + ".png"))
+                    os.remove(os.path.join(self.in_path_0, item + ".png"))
+                    os.remove(os.path.join(self.in_path_1, item + '.png'))
                 
                 elif item in y_train_files:
 
                     os.remove(os.path.join(out_path, item + ".npy"))
 
         # ^^ removing files present in one (x, y) and not other (x, y) to prevent dataset misalignment
-        '''
+        
 
         self.x_train_0_paths = []
         self.x_train_1_paths = []
@@ -313,11 +314,221 @@ class PairedDatasetDouble: #dataset for double view neural network model
 
     def prepare(self):
         
-        self.x_train_0 = np.reshape(self.x_train_0, (len(self.x_train_0), 256, 256, 1))
-        self.x_train_1 = np.reshape(self.x_train_1, (len(self.x_train_1), 256, 256, 1))
+        self.x_train_0 = np.reshape(self.x_train_0, (len(self.x_train_0), 1024, 1024, 1))
+        self.x_train_1 = np.reshape(self.x_train_1, (len(self.x_train_1), 1024, 1024, 1))
         self.y_train = np.reshape(self.y_train, (len(self.y_train), 128, 128, 128, 1))
         print("Shape of X0: {}".format(self.x_train_0.shape))
         print("Shape of X1: {}".format(self.x_train_1.shape))
         print("Shape of Y: {}".format(self.y_train.shape))
 
-pd = PairedDatasetDouble('Data/In/0', 'Data/In/1', 'Data/Out_New', 19)
+class PairedDatasetQuad: #dataset for double view neural network model
+
+    def __init__(self, in_path_0, in_path_1, in_path_2, in_path_3, out_path, filelimiter): #if filelimiter is -1 or > 943, unlimited.
+        
+        self.in_path_0 = in_path_0
+        self.in_path_1 = in_path_1
+        self.in_path_2 = in_path_2
+        self.in_path_3 = in_path_3
+        self.out_path = out_path
+
+        self.x_train_0 = []
+        self.x_train_1 = []
+        self.x_train_2 = []
+        self.x_train_3 = []
+        self.y_train = []
+
+        self.x_train_0_files = os.listdir(in_path_0)
+        self.x_train_1_files = os.listdir(in_path_1)
+        self.x_train_2_files = os.listdir(in_path_2)
+        self.X_train_3_files = os.listdir(in_path_3)
+        self.y_train_files = os.listdir(out_path)
+
+        for i in range(0, len(self.x_train_0_files)):
+
+            self.x_train_0_files[i] = self.x_train_0_files[i][0:4]
+
+        for i in range(0, len(self.x_train_1_files)):
+
+            self.x_train_1_files[i] = self.x_train_1_files[i][0:4]
+
+        for i in range(0, len(self.x_train_2_files)):
+
+            self.x_train_2_files[i] = self.x_train_2_files[i][0:4]
+
+        for i in range(0, len(self.x_train_3_files)):
+
+            self.x_train_3_files[i] = self.x_train_3_files[i][0:4]
+
+        for i in range(0, len(self.y_train_files)):
+
+            self.y_train_files[i] = self.y_train_files[i][0:4]
+
+        print("Length of x_train 0 files: {}".format(len(self.x_train_0_files)))
+        print("Length of x_train 1 files: {}".format(len(self.x_train_1_files)))
+        print("Length of x_train 2 files: {}".format(len(self.x_train_2_files)))
+        print("Length of x_train 3 files: {}".format(len(self.x_train_3_files)))
+        print("Length of y_train files: {}".format(len(self.y_train_files)))
+
+        main_list = np.setdiff1d(self.x_train_0_files, self.y_train_files) #assuming that corresponding 0, 1, 2, 3 views are inputted
+
+        for item in main_list:
+
+            if (item[0] != '.'):
+
+                if item in self.x_train_0_files:
+
+                    os.remove(os.path.join(self.in_path_0, item + ".png"))
+                    os.remove(os.path.join(self.in_path_1, item + '.png'))
+                    os.remove(os.path.join(self.in_path_2, item + '.png'))
+                    os.remove(os.path.join(self.in_path_3, item + '.png'))
+                
+                elif item in y_train_files:
+
+                    os.remove(os.path.join(out_path, item + ".npy"))
+
+        # ^^ removing files present in one (x, y) and not other (x, y) to prevent dataset misalignment
+        
+
+        self.x_train_0_paths = []
+        self.x_train_1_paths = []
+        self.x_train_2_paths = []
+        self.x_train_3_paths = []
+        self.y_train_paths = []
+
+        for item in os.listdir(self.in_path_0):
+
+            if (item[-3: ] == 'png'):
+
+                self.x_train_0_paths.append(item)
+
+        for item in os.listdir(self.in_path_1):
+
+            if (item[-3: ] == 'png'):
+
+                self.x_train_1_paths.append(item)
+
+        for item in os.listdir(self.in_path_2):
+
+            if (item[-3:] == 'png'):
+
+                self.x_train_2_paths.append(item)
+
+        for item in os.listdir(self.in_path_3):
+
+            if (item[-3:] == 'png'):
+
+                self.x_train_3_paths.append(item)
+
+        for item in os.listdir(out_path):
+
+            if (item[-3: ] == 'npy'):
+
+                self.y_train_paths.append(item)
+
+        self.x_train_0_paths = sorted(self.x_train_0_paths)
+        self.x_train_1_paths = sorted(self.x_train_1_paths)
+        self.x_train_2_paths = sorted(self.x_train_2_paths)
+        self.x_train_3_paths = sorted(self.x_train_3_paths)
+        self.y_train_paths = sorted(self.y_train_paths)
+
+        if (filelimiter < 1 or filelimiter> len(self.x_train_0_paths)):
+
+            filelimiter = len(self.x_train_0_paths)
+
+        for i in range(0, filelimiter):
+
+            pt_in_0 = os.path.join(in_path_0, self.x_train_0_paths[i])
+            pt_in_1 = os.path.join(in_path_1, self.x_train_1_paths[i])
+            pt_in_2 = os.path.join(in_path_2, self.x_train_2_paths[i])
+            pt_in_3 = os.path.join(in_path_3, self.x_train_3_paths[i])
+
+            pt_out = os.path.join(out_path, self.y_train_paths[i])
+
+            self.x_train_0.append(cv.imread(pt_in_0, cv.IMREAD_GRAYSCALE))
+            self.x_train_1.append(cv.imread(pt_in_1, cv.IMREAD_GRAYSCALE))
+            self.x_train_2.append(cv.imread(pt_in_2, cv.IMREAD_GRAYSCALE))
+            self.x_train_3.append(cv.imread(pt_in_3, cv.IMREAD_GRAYSCALE))
+            self.y_train.append(np.load(pt_out))
+
+        self.x_train_0 = np.array(self.x_train_0).astype('float64')
+        self.x_train_0 /= np.amax(self.x_train_0)
+
+        self.x_train_1 = np.array(self.x_train_1).astype('float64')
+        self.x_train_1 /= np.amax(self.x_train_1)
+
+        self.x_train_2 = np.array(self.x_train_2).astype('float64')
+        self.x_train_2 /= np.amax(self.x_train_2)
+
+        self.x_train_3 = np.array(self.x_train_3).astype('float64')
+        self.x_train_3 /= np.amax(self.x_train_3)
+        
+        for i in range(0, len(self.y_train)):
+
+            item = self.y_train[i]
+            print("Item {} with max: {} and min: {}".format(i, np.amax(self.y_train[i]), np.amin(self.y_train[i])))
+            normalized = normalize(item)
+            self.y_train[i] = normalized
+            print("Normalized item {} with max: {} and min: {}".format(i, np.amax(self.y_train[i]), np.amin(self.y_train[i])))
+        
+        self.y_train = np.array(self.y_train).astype('float64')
+
+
+    def view_pair(self, index):
+
+        f = plt.figure()
+
+        f.add_subplot(1, 5, 1)
+        plt.imshow(self.x_train_0[index], cmap = 'gray')
+        plt.title('Theta 0 of {}'.format(index))
+
+        f.add_subplot(1, 5, 2)
+        plt.imshow(self.x_train_1[index], cmap = 'gray')
+        plt.title('Theta 1 of {}'.format(index))
+
+        f.add_subplot(1, 5, 3)
+        plt.imshow(self.x_train_2[index], cmap = 'gray')
+        plt.title('Theta 2 of {}'.format(index))
+
+        f.add_subplot(1, 5, 4)
+        plt.imshow(self.x_train_3[index], cmap = 'gray')
+        plt.title('Theta 3 of {}'.format(index))
+
+        f.add_subplot(1, 5, 5)
+        plt.imshow(self.y_train[index][64], cmap = 'gray')
+        plt.title("Tomography of {}".format(index))
+        plt.show(block = True)
+
+    def view_animation(self, index):
+
+        if (self.y_train):
+
+            array = self.y_train[index]
+
+            ims = []
+
+            for im in array:
+
+                gg = plt.imshow(im, cmap = 'gray')
+                ims.append([gg])
+
+            fig = plt.figure()
+            ani = animation.ArtistAnimation(fig, ims, interval = 50, blit = True)
+            plt.title(index)
+            plt.show()
+
+        else:
+
+            print("Error: dataset has not been initialized yet")
+
+    def prepare(self):
+        
+        self.x_train_0 = np.reshape(self.x_train_0, (len(self.x_train_0), 1024, 1024, 1))
+        self.x_train_1 = np.reshape(self.x_train_1, (len(self.x_train_1), 1024, 1024, 1))
+        self.x_train_2 = np.reshape(self.x_train_2, (len(self.x_train_2), 1024, 1024, 1))
+        self.x_train_3 = np.reshape(self.x_train_3, (len(selx.x_train_3), 1024, 1024, 1))
+        self.y_train = np.reshape(self.y_train, (len(self.y_train), 128, 128, 128, 1))
+        print("Shape of X0: {}".format(self.x_train_0.shape))
+        print("Shape of X1: {}".format(self.x_train_1.shape))
+        print("Shape of X2: {}".format(self.x_train_2.shape))
+        print("Shape of X3: {}".format(self.x_train_3.shape))
+        print("Shape of Y: {}".format(self.y_train.shape))
