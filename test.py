0519ece568f78e9bd783030f13a126694523c6c5
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-10-25 13:17:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv.imread('Data/In/0/0001.png', cv.IMREAD_GRAYSCALE)
-img = img / 255
-img = 1 - img
-plt.imshow(img, cmap = 'gray')
-plt.show()
+array = np.load('Data/Out/0001.npy')
+array = np.clip(array, -1000, 3000)
+sagittal = array[128]
+coronal = array[:, 256]
+axial = array[:, :, 256]
 
-plt.imsave('inverted.png', img, cmap = 'gray')
+plt.imsave('sagittal.png', sagittal, cmap = 'gray', vmin = -1000, vmax = 3000)
+plt.imsave('coronal.png', coronal, cmap = 'gray', vmin = -1000, vmax = 3000)
+plt.imsave('axial.png', axial, cmap = 'gray', vmin = -1000, vmax = 3000)
+#plt.imsave('inverted.png', img, cmap = 'gray')
